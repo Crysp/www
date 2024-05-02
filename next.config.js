4a8810c5a9b8eeac09f78d176ae343549f41b208
Crysp <just.lyonya@gmail.com>
2024-05-03 00:03:01 +0400

    Generated pages

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
+const development = process.env.NODE_ENV !== 'production';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export'
+  output: 'export',
+  distDir: 'docs',
+  assetPrefix: development ? '' : 'https://crysp.github.io/www/'
 };
 
 export default nextConfig;
