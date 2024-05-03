b80e5c6a7a8c7d75f6e7af4a5b1b37dcc75f4632
Crysp <just.lyonya@gmail.com>
2024-05-03 19:35:28 +0400

    Generated pages

--- docs/_next/static/sITQewak9Zi-dmCteOjgT/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002Fhandbook","\u002Fhandbook\u002F[id]","\u002Fmessages","\u002Fmessages\u002F[id]","\u002Fnotes","\u002Fnotes\u002F[id]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
-const development = process.env.NODE_ENV !== 'production';
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  distDir: 'docs',
-  assetPrefix: development ? '' : 'https://github.com/Crysp/www/blob/main/docs/'
+  output: 'export'
 };
 
 export default nextConfig;
